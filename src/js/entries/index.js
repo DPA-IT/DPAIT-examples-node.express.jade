a7cfd4bbd5bfbf0df8e366e68c2d663147838f4a
siparsons <simon.parsons@retig.co.uk>
2017-01-20 12:09:45 +0000

    introduce submit button

--- src/js/entries/index.js
+++ src/js/entries/index.js
@@ -4,13 +4,31 @@ import nav from '../../stylus/modules/nav.styl'
 import hero from '../../stylus/modules/hero.styl'
 
 import dpa from 'dpa-it'
+import $ from 'jquery'
 
-/* ##### JS ##### */
-console.log('Home Page')
+$(document).ready(function () {
 
-var options = {
+	var dpaIt = new dpa('GHYT-32423432-KJHKJHK-123123123-JHG');
 
-};
+	dpaIt.init('#dpa-container', {
+		input_css	: "myCustomClass"
+	});
 
-var dpaIt = new dpa('#dpa-container', options);
-dpaIt.init();
+	var submission = {
+		customerId	: "ABC123",
+		firstName 	: "Simon", 
+		lastName 	: "Parsons", 
+		email 		: "simon.parsons@sipsoftware.co.uk", 
+		mobNumber  	: "+44(0)7721532678"
+	};
+
+	$('#submit').click(function(){
+
+		dpaIt.save(submission, 
+			function(submission_id) {
+				console.log("saved submission " + submission_id);
+			}
+		);
+	})
+});
+
