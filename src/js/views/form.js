5ec732f894f8778bf0901127810cde63ab863539
siparsons <simon.parsons@retig.co.uk>
2017-02-13 10:37:08 +0000

    changes

--- src/js/views/form.js
+++ src/js/views/form.js
@@ -1,15 +1,16 @@
 import $ from 'jquery'
+import moment from 'moment'
 
 import "az-styles";
 import "bootstrap";
 
 import "../functions.js"
 
-import dpa from "dpa-it";
+import ticki from "dpa-it";
 
 $(document).ready(function () {
 
-	var dpaIt = new dpa('GHYT-32423432-KJHKJHK-123123123-JHG');
+	var ticki = new ticki('NThhMDY3NTU2ZjdlZjQwMTAwODQwOGFi');
 
 	dpaIt.init('#dpa-container', {
 		form_group			: "form-group form-group-lg", 
@@ -19,14 +20,14 @@ $(document).ready(function () {
 	$('#submit').click(function(){
 
 		var submission = {
-			customerId	: "<Insert_Customer_Id_Here>",
+			customerId	: moment().format('YYYYDDMMHHms'),
 			firstName 	: document.getElementById('first-name').value, 
 			lastName 	: document.getElementById('last-name').value, 
 			email 		: document.getElementById('email').value, 
 			mobNumber  	: document.getElementById('mob-tel').value
 		};
 
-		dpaIt.save(submission, 
+		ticki.save(submission, 
 			function(submission_id) {
 				console.log("saved submission " + submission_id);
 			}
